0f9309e67a6e96d245e3bcd1950f3f7a6cb20edd
LightScript <44749567+LightScr1pt@users.noreply.github.com>
2020-04-09 19:34:13 +0200

    Update Ports-Python.py

--- Ports-Python.py
+++ Ports-Python.py
@@ -7,7 +7,7 @@ except:
     os.system('pip3 instal python-nmap')
     os.system('clear')
 
-
+line = "---------------------------------------------------------------"
 
 if len(sys.argv)!=3:
     print(' python3 Ports-Python.py <Host_Address> <Ports_Range>')
@@ -18,29 +18,26 @@ portsrange = sys.argv[2]
 
 ipaddress = socket.gethostbyname(hostaddress)
 
-print('-' * 55)
+print(line)
 print('    Please wait... Now scaning ' + ipaddress)
-print('-' * 55)
+print(line)
 
 try:
     nmscan=nmap.PortScanner()
     nmscan.scan(hostaddress, portsrange)
 
-
 except:
-    print('    Error, Program Stoped ')
+    print('    Program Stoped...')
     sys.exit(0)
 
 for host in nmscan.all_hosts():
-    print('    Host : %s (%s)' % (host, hostaddress))
-    print('    Status : %s' % nmscan[host].state())
+    print('    Host :      |%s(%s)' % (host, hostaddress))
+    print('    Status :    |%s' % nmscan[host].state())
     for proto in nmscan[host].all_protocols():
-        print('-' * 55)
-        print('    Protocols : %s ' % proto)
-
+        print(line)
+        print('    Protocols : |%s ' % proto)
+        
         lport = nmscan[host][proto].keys()
         sorted(lport)
         for port in lport:
-            print('    Port : %s \t State : %s ' %(port,nmscan[host][proto][port]['state']))
-
-
+            print('    Port :      |%s \t State : %s ' %(port,nmscan[host][proto][port]['state']))
